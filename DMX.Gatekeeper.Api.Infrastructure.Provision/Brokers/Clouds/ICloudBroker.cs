32730a494a4717331b17057da572bcb126fbca4e
Terry Palomares <tepaloma@microsoft.com>
2022-05-20 15:36:55 -0700

    INFRA: Initialize Cloud Broker

---
+++ DMX.Gatekeeper.Api.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMX.Gatekeeper.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+    }
+}
