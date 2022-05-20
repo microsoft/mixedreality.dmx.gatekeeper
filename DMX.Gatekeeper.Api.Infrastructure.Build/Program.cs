4e4a5878357fe5cebd1b90a43de282debcf81064
Terry Palomares <tepaloma@microsoft.com>
2022-05-20 11:06:12 -0700

    INFRA: Build Steps

---
+++ DMX.Gatekeeper.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,63 @@
+﻿using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Gatekeeper Build",
+
+    OnEvents = new Events
+    {
+        Push = new PushEvent
+        {
+            Branches = new string[] { "master" }
+        },
+
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "maser" }
+        }
+    }
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.Windows2022,
+
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Checking out Code"
+                },
+
+                new SetupDotNetTaskV1
+                {
+                    Name = "Installing .NET",
+
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion = "7.0.100-preview.4.22252.9",
+                        IncludePrerelease = true
+                    }
+                },
+
+                new RestoreTask
+                {
+                    Name = "Restoring Packages"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "Building Project(s)"
+                },
+
+                new TestTask
+                {
+                    Name = "Running Tests"
+                }
+            }
+        }
+    }
+};
