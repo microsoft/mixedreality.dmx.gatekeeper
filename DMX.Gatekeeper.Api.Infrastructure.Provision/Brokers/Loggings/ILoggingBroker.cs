5950b727f2fa380c8293ce5af92390c9494173e6
Hassan Habib <harezk@microsoft.com>
2022-05-25 13:46:24 -0700

    BROKERS: Implement Logging Broker

---
+++ DMX.Gatekeeper.Api.Infrastructure.Provision/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace DMX.Gatekeeper.Api.Infrastructure.Provision.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogActivity(string message);
+    }
+}
