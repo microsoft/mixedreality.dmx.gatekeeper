37e35302ef2121e742c4e00451e288e318c8e75a
Hassan Habib <harezk@microsoft.com>
2022-05-25 13:31:10 -0700

    BROKERS: Implement Plans

---
+++ DMX.Gatekeeper.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.Plans.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+namespace DMX.Gatekeeper.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker
+    {
+        public async ValueTask<IAppServicePlan> CreatePlanAsync(
+            string planName,
+            IResourceGroup resourceGroup)
+        {
+            return await this.azure.AppServices.AppServicePlans
+                .Define(planName)
+                .WithRegion(Region.USEast2)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithPricingTier(PricingTier.StandardS1)
+                .WithOperatingSystem(OperatingSystem.Windows)
+                .CreateAsync();
+        }
+    }
+}
