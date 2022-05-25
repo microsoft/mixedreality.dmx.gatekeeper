923154435aac0b6786c47a83780d023370b24a91
Hassan Habib <harezk@microsoft.com>
2022-05-25 13:34:12 -0700

    BROKERS: Implement Web Apps

---
+++ DMX.Gatekeeper.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.WebApps.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace DMX.Gatekeeper.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker
+    {
+        public async ValueTask<IWebApp> CreateWebAppAsync(
+            string webAppName,
+            string connectionString,
+            IAppServicePlan appServicePlan,
+            IResourceGroup resourceGroup)
+        {
+            return await this.azure.AppServices.WebApps
+                .Define(webAppName)
+                .WithExistingWindowsPlan(appServicePlan)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithNetFrameworkVersion(NetFrameworkVersion.Parse("v7.0"))
+                .CreateAsync();
+        }
+    }
+}
---
+++ DMX.Gatekeeper.Api.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.WebApps.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace DMX.Gatekeeper.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<IWebApp> CreateWebAppAsync(
+            string webAppName,
+            string connectionString,
+            IAppServicePlan appServicePlan,
+            IResourceGroup resourceGroup);
+    }
+}
