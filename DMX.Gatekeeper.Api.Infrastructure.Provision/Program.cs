406014a3d21f847c33387de12438ade720fc56c0
Hassan Habib <harezk@microsoft.com>
2022-05-25 13:25:41 -0700

    BROKER: Initialize Cloud Broker

---
+++ DMX.Gatekeeper.Api.Infrastructure.Provision/Program.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace DMX.Gatekeeper.Api.Infrastructure.Provision
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
