9e2ba378934aca5a0a3b3e8364e414da8e383516
Hassan Habib <harezk@microsoft.com>
2022-05-25 13:28:20 -0700

    BROKERS: Implement Cloud Broker

---
+++ DMX.Gatekeeper.Api.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.ResourceGroups.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace DMX.Gatekeeper.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<bool> CheckResourceGroupExistsAsync(string resourceGroupName);
+        ValueTask<IResourceGroup> CreateResourceGroupAsync(string resourceGroupName);
+        ValueTask DeleteResourceGroupAsync(string resourceGroupName);
+    }
+}
