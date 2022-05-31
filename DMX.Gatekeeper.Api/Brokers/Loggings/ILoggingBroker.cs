5cf2ea08ee94b7f652925d63b78246cb567390b6
Andrew McClelland <andrew.mcclelland@microsoft.com>
2022-05-31 14:19:01 -0700

    CODE RUB: Remove extra lines in Logging Broker interface

--- DMX.Gatekeeper.Api/Brokers/Loggings/ILoggingBroker.cs
+++ DMX.Gatekeeper.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -9,15 +9,10 @@ namespace DMX.Gatekeeper.Api.Brokers.Loggings
     public interface ILoggingBroker
     {
         void LogInformation(string message);
-
         void LogTrace(string message);
-
         void LogDebug(string message);
-
         void LogWarning(string message);
-
         void LogError(Exception exception);
-
         void LogCritical(Exception exception);
     }
 }
