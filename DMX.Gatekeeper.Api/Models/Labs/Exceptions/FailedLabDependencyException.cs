e2a28ce58e62d1ff6db892ccf4429b6b07eb6068
Hassan Habib <harezk@microsoft.com>
2022-05-31 15:07:31 -0700

    ShouldThrowCriticalDependencyExceptionOnRetrievalIfCriticalErrorOccursAndLogItAsync -> FAIL

---
+++ DMX.Gatekeeper.Api/Models/Labs/Exceptions/FailedLabDependencyException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace DMX.Gatekeeper.Api.Models.Labs.Exceptions
+{
+    public class FailedLabDependencyException : Xeption
+    {
+        public FailedLabDependencyException(Exception innerException)
+            : base(message: "Failed lab dependency error occured, contact support.",
+                  innerException)
+        { }
+    }
+}
