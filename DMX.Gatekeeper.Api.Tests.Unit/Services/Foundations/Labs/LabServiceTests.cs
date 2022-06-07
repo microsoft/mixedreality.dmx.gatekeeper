7856e6a8b717ef108af7cf2e53268778a99d2f70
Andrew McClelland <andrew.mcclelland@microsoft.com>
2022-06-07 10:05:56 -0700

    CODE RUB: Update 'Xeption' NuGet Package and Use SameExceptionAs

--- DMX.Gatekeeper.Api.Tests.Unit/Services/Foundations/Labs/LabServiceTests.cs
+++ DMX.Gatekeeper.Api.Tests.Unit/Services/Foundations/Labs/LabServiceTests.cs
@@ -48,13 +48,8 @@ namespace DMX.Gatekeeper.Api.Tests.Unit.Services.Foundations.Labs
             };
         }
 
-        private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException)
-        {
-            return actualException =>
-                actualException.Message == expectedException.Message &&
-                actualException.InnerException.Message == expectedException.InnerException.Message &&
-                (actualException.InnerException as Xeption).DataEquals(expectedException.InnerException.Data);
-        }
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
 
         private static List<Lab> CreateRandomLabs() =>
             CreateLabFiller().Create(count: GetRandomNumber()).ToList();
