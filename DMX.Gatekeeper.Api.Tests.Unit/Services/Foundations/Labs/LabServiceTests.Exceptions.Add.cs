0d7f9cca29fbed8a82e885fb63829d89794aba35
Geetika Jain <geetsk2194@gmail.com>
2022-06-22 15:54:12 -0700

    ShouldThrowDependencyExceptionOnAddIfErrorOccursAndLogItAsync -> FAIL

--- DMX.Gatekeeper.Api.Tests.Unit/Services/Foundations/Labs/LabServiceTests.Exceptions.Add.cs
+++ DMX.Gatekeeper.Api.Tests.Unit/Services/Foundations/Labs/LabServiceTests.Exceptions.Add.cs
@@ -6,6 +6,8 @@ using DMX.Gatekeeper.Api.Models.Labs;
 using DMX.Gatekeeper.Api.Models.Labs.Exceptions;
 using FluentAssertions;
 using Moq;
+using RESTFulSense.Exceptions;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xeptions;
 using Xunit;
@@ -50,6 +52,51 @@ namespace DMX.Gatekeeper.Api.Tests.Unit.Services.Foundations.Labs
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(expectedLabDependencyException))),
                     Times.Once);
+
+            this.dmxApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddIfErrorOccursAndLogItAsync()
+        {
+            // given
+            Lab randomLab = CreateRandomLab();
+            string randomMessage = GetRandomString();
+            var httpMessage = new HttpResponseMessage();
+
+            var httpResponseException = 
+                new HttpResponseException(httpMessage, randomMessage);
+
+            var failedLabDependencyException =
+                new FailedLabDependencyException(httpResponseException);
+
+            var expectedLabDependencyException =
+                new LabDependencyException(failedLabDependencyException);
+
+            this.dmxApiBrokerMock.Setup(broker =>
+                broker.PostLabAsync(It.IsAny<Lab>()))
+                    .ThrowsAsync(httpResponseException);
+
+            // when
+            ValueTask<Lab> addLabTask =
+                this.labService.AddLabAsync(randomLab);
+
+            LabDependencyException actualLabDependencyException =
+                await Assert.ThrowsAsync<LabDependencyException>(addLabTask.AsTask);
+
+            //then
+            actualLabDependencyException.Should()
+                .BeEquivalentTo(expectedLabDependencyException);
+
+            this.dmxApiBrokerMock.Verify(broker =>
+                broker.PostLabAsync(It.IsAny<Lab>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(
+                    SameExceptionAs(expectedLabDependencyException))),
+                        Times.Once);
 
             this.dmxApiBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
