02dd7f13e09b860645ecadd7a7e98595e0f377d5
Geetika Jain <geetsk2194@gmail.com>
2022-06-22 14:52:02 -0700

    ShouldThrowValidationExceptionOnAddIfLabIsNullAndLogItAsync -> FAIL

---
+++ DMX.Gatekeeper.Api.Tests.Unit/Services/Foundations/Labs/LabServiceTests.Validation.Add.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMX.Gatekeeper.Api.Models.Labs;
+using DMX.Gatekeeper.Api.Models.Labs.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DMX.Gatekeeper.Api.Tests.Unit.Services.Foundations.Labs
+{
+    public partial class LabServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfLabIsNullAndLogItAsync()
+        {
+            // given
+            Lab nullLab = null;
+
+            var nullException = new NullLabException();
+
+            var expectedValidationException = 
+                new LabValidationException(nullException);
+
+            // when
+            var addLabTask = 
+                this.labService.AddLabAsync(nullLab);
+
+            var actualValidationException =
+                await Assert.ThrowsAsync<LabValidationException>(addLabTask.AsTask);
+
+            // then
+            actualValidationException.Should().BeEquivalentTo(
+                expectedValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedValidationException))),
+                    Times.Once);
+
+            this.dmxApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ DMX.Gatekeeper.Api/Models/Labs/Exceptions/LabValidationException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace DMX.Gatekeeper.Api.Models.Labs.Exceptions
+{
+    public class LabValidationException : Xeption
+    {
+        public LabValidationException(Xeption innerException)
+            : base(message: "Lab validation error occurred. Please fix it and try again",
+                  innerException)
+        {
+        }
+    }
+}
