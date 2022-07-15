6836ab39a3ee8707cece15db1ec1fd8b1dd6e8dd
Terry Palomares <tpalomares18@gmail.com>
2022-07-15 10:15:06 -0700

    BROKERS: Add token acquisition

---
+++ DMX.Gatekeeper.Api/Brokers/Tokens/ITokenAcquisitionBroker.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace DMX.Gatekeeper.Api.Brokers.Tokens
+{
+    public partial interface ITokenAcquisitionBroker
+    { }
+}
---
+++ DMX.Gatekeeper.Api/Brokers/Tokens/TokenAcquisitionBroker.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Web;
+
+namespace DMX.Gatekeeper.Api.Brokers.Tokens
+{
+    public partial class TokenAcquisitionBroker : ITokenAcquisitionBroker
+    {
+        private ITokenAcquisition tokenAcquisition;
+
+        public TokenAcquisitionBroker(
+            ITokenAcquisition tokenAcquisition, 
+            IConfiguration configuration)
+        {
+            this.tokenAcquisition = tokenAcquisition;
+            
+        }
+
+        public async ValueTask<string> GetAccessTokenForUserAsync(string[] scopes) =>
+            await tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+        }
+}
