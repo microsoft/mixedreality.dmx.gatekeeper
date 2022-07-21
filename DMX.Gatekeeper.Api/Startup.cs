0b9b7c8b990679de97fef02c83ceb8343d9dd47d
Hassan Habib <harezk@microsoft.com>
2022-07-20 17:56:48 -0700

    CODE RUB: Run Code Cleanup

--- DMX.Gatekeeper.Api/Startup.cs
+++ DMX.Gatekeeper.Api/Startup.cs
@@ -14,7 +14,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Authentication;
 
 namespace DMX.Gatekeeper.Api
 {
