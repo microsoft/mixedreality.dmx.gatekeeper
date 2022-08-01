fbe3b347653c8800a25790615da1340737a3ba74
Michael Mendelsohn <jazzapple42@gmail.com>
2022-08-01 10:39:02 -0700

    DATA: Lab Commands

---
+++ DMX.Gatekeeper.Api/Models/Commands/CommandType.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace DMX.Gatekeeper.Api.Models.Commands
+{
+    public enum CommandType
+    {
+        Download,
+        Execute
+    }
+}
