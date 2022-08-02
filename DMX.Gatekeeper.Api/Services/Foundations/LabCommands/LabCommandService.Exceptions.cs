f8eb18c28ec4da8ddac6ea86747c8f00931bb52e
Michael Mendelsohn <jazzapple42@gmail.com>
2022-08-02 15:53:27 -0700

    ShouldThrowCriticalDependencyExceptionOnAddIfCriticalErrorOccursAndLogItAsync -> FAIL

--- DMX.Gatekeeper.Api/Services/Foundations/LabCommands/LabCommandService.Exceptions.cs
+++ DMX.Gatekeeper.Api/Services/Foundations/LabCommands/LabCommandService.Exceptions.cs
@@ -2,9 +2,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using DMX.Gatekeeper.Api.Models.LabCommands;
 using DMX.Gatekeeper.Api.Models.LabCommands.Exceptions;
+using RESTFulSense.Exceptions;
 using Xeptions;
 
 namespace DMX.Gatekeeper.Api.Services.Foundations.LabCommands
@@ -19,10 +21,39 @@ namespace DMX.Gatekeeper.Api.Services.Foundations.LabCommands
             {
                 return await returningLabCommand();
             }
+            catch (HttpResponseUrlNotFoundException httpResponseUrlNotFoundException)
+            {
+                var faliedLabCommandDependencyException =
+                    new FailedLabCommandDependencyException(httpResponseUrlNotFoundException);
+
+                throw CreateAndLogCriticalDependencyException(faliedLabCommandDependencyException);
+            }
+            catch (HttpResponseUnauthorizedException httpResponseUnauthorizedException)
+            {
+                var faliedLabCommandDependencyException =
+                    new FailedLabCommandDependencyException(httpResponseUnauthorizedException);
+
+                throw CreateAndLogCriticalDependencyException(faliedLabCommandDependencyException);
+            }
+            catch (HttpResponseForbiddenException httpResponseForbiddenException)
+            {
+                var faliedLabCommandDependencyException =
+                    new FailedLabCommandDependencyException(httpResponseForbiddenException);
+
+                throw CreateAndLogCriticalDependencyException(faliedLabCommandDependencyException);
+            }
             catch (NullLabCommandException nullLabCommandException)
             {
                 throw CreateAndLogValidationException(nullLabCommandException);
             }
+        }
+
+        private LabCommandDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var labCommandDependencyException = new LabCommandDependencyException(exception);
+            this.loggingBroker.LogCritical(labCommandDependencyException);
+
+            return labCommandDependencyException;
         }
 
         private LabCommandValidationException CreateAndLogValidationException(Xeption exception)
