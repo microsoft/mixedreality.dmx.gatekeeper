c8d374628002a28fcb3b21149c00392884e49759
Michael Mendelsohn <jazzapple42@gmail.com>
2022-08-02 11:17:00 -0700

    CODE RUB: Make RequiredScope Usings Release Only

--- DMX.Gatekeeper.Api/Controllers/LabsController.cs
+++ DMX.Gatekeeper.Api/Controllers/LabsController.cs
@@ -9,14 +9,15 @@ using DMX.Gatekeeper.Api.Models.Labs.Exceptions;
 using DMX.Gatekeeper.Api.Services.Foundations.Labs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Identity.Web.Resource;
 using RESTFulSense.Controllers;
+
+#if RELEASE
+using Microsoft.Identity.Web.Resource;
+#endif
 
 namespace DMX.Gatekeeper.Api.Controllers
 {
-#if RELEASE
     [Authorize]
-#endif
     [ApiController]
     [Route("api/[controller]")]
     public class LabsController : RESTFulController
