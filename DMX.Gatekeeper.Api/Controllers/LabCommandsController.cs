4c8cb20eec1f730f0bd2d47820c199dec013c5d9
Terry Palomares <tpalomares18@gmail.com>
2022-08-08 10:29:27 -0700

    CONTROLLERS: PostLabCommand

---
+++ DMX.Gatekeeper.Api/Controllers/LabCommandsController.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using DMX.Gatekeeper.Api.Models.LabCommands;
+using DMX.Gatekeeper.Api.Models.Labs.Exceptions;
+using DMX.Gatekeeper.Api.Services.Foundations.LabCommands;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+#if RELEASE
+using Microsoft.Identity.Web.Resource;
+#endif
+
+namespace DMX.Gatekeeper.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LabCommandsController : RESTFulController
+    {
+        private readonly ILabCommandService labCommandService;
+
+        public LabCommandsController(ILabCommandService labCommandService) =>
+            this.labCommandService = labCommandService;
+        
+        [HttpPost]
+#if RELEASE
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes:PostLabCommand")]
+#endif
+        public async ValueTask<ActionResult<LabCommand>> PostLabCommandAsync(LabCommand labCommand)
+        {
+            try
+            {
+                LabCommand addLabCommand =
+                    await this.labCommandService.AddLabCommandAsync(labCommand);
+
+                return Created(addLabCommand);
+            }
+            catch (LabValidationException labValidationException)
+            {
+                return BadRequest(labValidationException.InnerException);
+            }
+            catch (LabDependencyException labDependencyException)
+            {
+                return InternalServerError(labDependencyException);
+            }
+            catch (LabDependencyValidationException labDependencyValidationException)
+            {
+                return BadRequest(labDependencyValidationException.InnerException);
+            }
+            catch (LabServiceException labServiceException)
+            {
+                return InternalServerError(labServiceException);
+            }
+        }
+    }
+}
