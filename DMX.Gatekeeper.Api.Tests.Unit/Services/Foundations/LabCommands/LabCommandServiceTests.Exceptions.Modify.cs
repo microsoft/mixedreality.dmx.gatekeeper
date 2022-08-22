16161b0601644ee18a7778987ba05615d74c73dc
Michael Mendelsohn <jazzapple42@gmail.com>
2022-08-22 12:03:38 -0700

    ShouldThrowCriticalDependencyExceptionOnModifyIfCriticalErrorOccursAndLogItAsync -> PASS

---
+++ DMX.Gatekeeper.Api.Tests.Unit/Services/Foundations/LabCommands/LabCommandServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using DMX.Gatekeeper.Api.Models.LabCommands;
+using DMX.Gatekeeper.Api.Models.LabCommands.Exceptions;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xeptions;
+using Xunit;
+
+namespace DMX.Gatekeeper.Api.Tests.Unit.Services.Foundations.LabCommands
+{
+    public partial class LabCommandServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyException))]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfCriticalErrorOccursAndLogItAsync(
+            Xeption criticalDependencyException)
+        {
+            // given
+            LabCommand someLabCommand = CreateRandomLabCommand();
+
+            var failedLabCommandDependencyException =
+                new FailedLabCommandDependencyException(criticalDependencyException);
+
+            var expectedLabCommandDependencyException =
+                new LabCommandDependencyException(failedLabCommandDependencyException);
+
+            this.dmxApiBrokerMock.Setup(broker =>
+                broker.UpdateLabCommandAsync(It.IsAny<LabCommand>()))
+                    .ThrowsAsync(criticalDependencyException);
+
+            // when
+            ValueTask<LabCommand> modifyLabCommandTask =
+                this.labCommandService.ModifyLabCommandAsync(someLabCommand);
+
+            LabCommandDependencyException actualLabCommandDependencyException =
+                await Assert.ThrowsAsync<LabCommandDependencyException>(
+                    modifyLabCommandTask.AsTask);
+
+            // then
+            actualLabCommandDependencyException.Should().BeEquivalentTo(
+                expectedLabCommandDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedLabCommandDependencyException))),
+                        Times.Once);
+
+            this.dmxApiBrokerMock.Verify(broker =>
+                broker.UpdateLabCommandAsync(It.IsAny<LabCommand>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dmxApiBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
