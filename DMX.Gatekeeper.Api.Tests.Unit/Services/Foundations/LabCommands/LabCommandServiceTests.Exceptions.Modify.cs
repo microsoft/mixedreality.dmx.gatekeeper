f2ba42de9e27e541c1067af7eac2cacd6bab332a
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-08-22 13:10:57 -0700

    ShouldThrowDependencyExceptionOnModifyIfErrorsOccurAndLogItAsync -> PASS

--- DMX.Gatekeeper.Api.Tests.Unit/Services/Foundations/LabCommands/LabCommandServiceTests.Exceptions.Modify.cs
+++ DMX.Gatekeeper.Api.Tests.Unit/Services/Foundations/LabCommands/LabCommandServiceTests.Exceptions.Modify.cs
@@ -2,15 +2,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Net.Http;
+using System.Threading.Tasks;
 using DMX.Gatekeeper.Api.Models.LabCommands;
 using DMX.Gatekeeper.Api.Models.LabCommands.Exceptions;
 using FluentAssertions;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using RESTFulSense.Exceptions;
 using Xeptions;
 using Xunit;
 
@@ -48,17 +46,63 @@ namespace DMX.Gatekeeper.Api.Tests.Unit.Services.Foundations.LabCommands
             actualLabCommandDependencyException.Should().BeEquivalentTo(
                 expectedLabCommandDependencyException);
 
+            this.dmxApiBrokerMock.Verify(broker =>
+                broker.UpdateLabCommandAsync(It.IsAny<LabCommand>()),
+                    Times.Once);
+            
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
                     expectedLabCommandDependencyException))),
                         Times.Once);
 
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dmxApiBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfErrorsOccurAndLogItAsync()
+        {
+            // given
+            LabCommand someLabCommand = CreateRandomLabCommand();
+            string randomMessage = GetRandomString();
+            var httpResponseMessage = new HttpResponseMessage();
+
+            var httpResponseException =
+                new HttpResponseException(httpResponseMessage, randomMessage);
+
+            var failedLabCommandDependencyException =
+                new FailedLabCommandDependencyException(httpResponseException);
+
+            var expectedLabCommandDependencyException =
+                new LabCommandDependencyException(failedLabCommandDependencyException);
+
+            this.dmxApiBrokerMock.Setup(broker =>
+                broker.UpdateLabCommandAsync(It.IsAny<LabCommand>()))
+                    .ThrowsAsync(httpResponseException);
+
+            // when
+            ValueTask<LabCommand> modifyLabCommandTask =
+                this.labCommandService.ModifyLabCommandAsync(someLabCommand);
+
+            LabCommandDependencyException actualLabCommandDependencyException =
+                await Assert.ThrowsAsync<LabCommandDependencyException>(
+                    modifyLabCommandTask.AsTask);
+
+            // then
+            actualLabCommandDependencyException.Should().BeEquivalentTo(
+                expectedLabCommandDependencyException);
+
             this.dmxApiBrokerMock.Verify(broker =>
                 broker.UpdateLabCommandAsync(It.IsAny<LabCommand>()),
                     Times.Once);
+            
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(
+                    SameExceptionAs(expectedLabCommandDependencyException))),
+                        Times.Once);
 
-            this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dmxApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
