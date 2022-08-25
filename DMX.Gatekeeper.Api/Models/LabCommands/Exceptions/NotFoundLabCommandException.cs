353bdc7249a13249121e4acee93b889ea8defa73
Geetika Jain <geetsk2194@gmail.com>
2022-08-25 12:55:36 -0700

    CODERUB: run code cleanup

--- DMX.Gatekeeper.Api/Models/LabCommands/Exceptions/NotFoundLabCommandException.cs
+++ DMX.Gatekeeper.Api/Models/LabCommands/Exceptions/NotFoundLabCommandException.cs
@@ -2,7 +2,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
-using System;
 using Xeptions;
 
 namespace DMX.Gatekeeper.Api.Models.LabCommands.Exceptions
