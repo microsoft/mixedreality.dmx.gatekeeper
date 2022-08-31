75adec478cbdf2e0e72b3fdf47357c0a07b91ff6
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-08-31 12:58:08 -0400

    BROKERS: Post New Artifact

---
+++ DMX.Gatekeeper.Api/Brokers/DmxApis/DmxApiBroker.Artifacts.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DMX.Gatekeeper.Api.Brokers.DmxApis
+{
+    public partial class DmxApiBroker
+    {
+        private const string ArtifactsRelativeUrl = "api/artifacts";
+
+        public async ValueTask PostArtifactAsync(Stream stream) =>
+            await PostAsync(ArtifactsRelativeUrl, stream);
+    }
+}
---
+++ DMX.Gatekeeper.Api/Brokers/DmxApis/IDmxApiBroker.Artifacts.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DMX.Gatekeeper.Api.Brokers.DmxApis
+{
+    public partial interface IDmxApiBroker
+    {
+        ValueTask PostArtifactAsync(Stream stream);
+    }
+}
