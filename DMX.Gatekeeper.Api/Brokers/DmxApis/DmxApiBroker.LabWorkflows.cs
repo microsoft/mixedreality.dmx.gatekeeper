209f2f79a56acea1a603b084795b83463a1d0799
Geetika Jain <geetsk2194@gmail.com>
2022-09-16 13:23:24 -0700

    BROKERS: Lab Workflow broker

---
+++ DMX.Gatekeeper.Api/Brokers/DmxApis/DmxApiBroker.LabWorkflows.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using DMX.Gatekeeper.Api.Models.LabWorkflows;
+
+namespace DMX.Gatekeeper.Api.Brokers.DmxApis
+{
+    public partial class DmxApiBroker
+    {
+        private const string LabWorkflowsRelativeUrl = "api/labworkflows";
+
+        public async ValueTask<LabWorkflow> PostLabWorkflowAsync(LabWorkflow labWorkflow) =>
+            await PostAsync(LabWorkflowsRelativeUrl, labWorkflow);
+
+        public async ValueTask<LabWorkflow> GetLabWorkflowByIdAsync(Guid id) =>
+            await GetAsync<LabWorkflow>($"{LabWorkflowsRelativeUrl}/{id}");
+
+        public async ValueTask<LabWorkflow> UpdateLabWorkflowAsync(LabWorkflow labWorkflow) =>
+            await UpdateAsync(LabWorkflowsRelativeUrl, labWorkflow);
+
+        public async ValueTask<LabWorkflow> DeleteLabWorkflowAsync(Guid id) =>
+            await DeleteAsync<LabWorkflow>($"{LabWorkflowsRelativeUrl}/{id}");
+    }
+}
